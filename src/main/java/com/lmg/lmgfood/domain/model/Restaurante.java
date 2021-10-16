619606ec3f15b048060e887813df8c0351f48fe1
lucas.maciel <lucas.maciel@compasso.com.br>
2021-10-16 10:02:22 -0300

    Refinando o payload de cadastro com @JsonIgnoreProperties

--- src/main/java/com/lmg/lmgfood/domain/model/Restaurante.java
+++ src/main/java/com/lmg/lmgfood/domain/model/Restaurante.java
@@ -31,6 +31,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -59,7 +60,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
-//	@JsonIgnore
+	@JsonIgnoreProperties(value = "nome", allowGetters = true) // na hora de desserializar o restaurante, ignorar o nome da propriedade cozinha, porém permite na serialização métodos getters
 	@Valid
 	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
 	@NotNull
