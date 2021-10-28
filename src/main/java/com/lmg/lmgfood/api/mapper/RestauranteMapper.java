1cec6d4492adff08bb3f894f6403865db7348ff1
lucas.maciel <lucas.maciel@compasso.com.br>
2021-10-28 10:57:09 -0300

    Utilizando modelMapper

--- src/main/java/com/lmg/lmgfood/api/mapper/RestauranteMapper.java
+++ src/main/java/com/lmg/lmgfood/api/mapper/RestauranteMapper.java
@@ -1,10 +1,10 @@
 package com.lmg.lmgfood.api.mapper;
 
-import com.lmg.lmgfood.api.model.CozinhaDTO;
 import com.lmg.lmgfood.api.model.RestauranteDTO;
 import com.lmg.lmgfood.api.model.form.RestauranteForm;
-import com.lmg.lmgfood.domain.model.Cozinha;
 import com.lmg.lmgfood.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,6 +13,9 @@ import java.util.stream.Collectors;
 @Component
 public class RestauranteMapper {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public List<RestauranteDTO> toCollectionDTO(List<Restaurante> restaurantes) {
         return restaurantes.stream()
                 .map(restaurante -> toDTO(restaurante))
@@ -20,27 +23,10 @@ public class RestauranteMapper {
     }
 
     public RestauranteDTO toDTO(Restaurante restaurante) {
-        CozinhaDTO cozinhaDTO = new CozinhaDTO();
-        cozinhaDTO.setId(restaurante.getCozinha().getId());
-        cozinhaDTO.setNome(restaurante.getCozinha().getNome());
-
-        RestauranteDTO dto = new RestauranteDTO();
-        dto.setId(restaurante.getId());
-        dto.setNome(restaurante.getNome());
-        dto.setTaxaFrete(restaurante.getTaxaFrete());
-        dto.setCozinha(cozinhaDTO);
-        return dto;
+        return modelMapper.map(restaurante, RestauranteDTO.class);
     }
 
     public Restaurante toDomain(RestauranteForm restauranteForm) {
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteForm.getCozinhaId().getId());
-
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteForm.getNome());
-        restaurante.setTaxaFrete(restauranteForm.getTaxaFrete());
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+        return modelMapper.map(restauranteForm, Restaurante.class);
     }
 }
