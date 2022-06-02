ada7e31451e60180e450bf3f33a766e0f8d39efa
lucasgois <lucasmacielgois@gmail.com>
2022-06-02 17:59:04 -0300

    Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/lmg/lmgfood/domain/repository/UsuarioRepository.java
+++ src/main/java/com/lmg/lmgfood/domain/repository/UsuarioRepository.java
@@ -4,7 +4,12 @@ import com.lmg.lmgfood.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
 	
 }
--- src/main/java/com/lmg/lmgfood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/lmg/lmgfood/domain/service/CadastroUsuarioService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CadastroUsuarioService {
@@ -27,6 +28,12 @@ public class CadastroUsuarioService {
 
     @Transactional
     public Usuario adicionar(Usuario usuario) {
+        usuarioRepository.detach(usuario);
+        Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)){
+            throw new NegocioException(String.format("Já existe um usuário com o e-mail %s", usuario.getEmail()));
+        }
         return usuarioRepository.save(usuario);
     }
 
