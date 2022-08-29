d0c9b6ed7782cf4803fdc1daa41310bc5f51e7ab
LucasMaciel.Gois-ext <LucasMaciel.Gois-ext@ab-Inbev.com>
2022-08-28 21:55:57 -0300

    Configurando autenticação de usuários em memória

--- src/main/java/com/lmg/lmgfood/core/security/WebSecurityConfig.java
+++ src/main/java/com/lmg/lmgfood/core/security/WebSecurityConfig.java
@@ -1,14 +1,36 @@
 package com.lmg.lmgfood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Configura uma autenticação em memória
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("lucas")
+                    .password(passwordEncoder().encode("123"))
+                    .roles("ADMIN")
+                .and()
+                .withUser("joao")
+                    .password(passwordEncoder().encode("123"))
+                    .roles();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,6 +44,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)// Não armazena estado na aplicação, não guarda cookie
                 .and()
                 .   csrf().disable();
+    }
+
+    /**
+     * Método para criptografar as senhas
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
