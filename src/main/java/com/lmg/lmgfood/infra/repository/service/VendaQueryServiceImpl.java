0e5e5141161f7089d5045f9e5f0ac4346d2317e1
LucasMaciel.Gois-ext <LucasMaciel.Gois-ext@ab-Inbev.com>
2022-10-03 15:47:28 -0300

    Tratando time offset na agregação de vendas diárias por data

--- src/main/java/com/lmg/lmgfood/infra/repository/service/VendaQueryServiceImpl.java
+++ src/main/java/com/lmg/lmgfood/infra/repository/service/VendaQueryServiceImpl.java
@@ -12,6 +12,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.Predicate;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -22,14 +23,19 @@ public class VendaQueryServiceImpl implements VendaQueryService {
     private EntityManager entityManager;
 
     @Override
-    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
+    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffSet) {
         var builder = entityManager.getCriteriaBuilder();
         var query = builder.createQuery(VendaDiaria.class);  // tipo que a consulta retorna
         var root = query.from(Pedido.class);
 
         var predicates = new ArrayList<Predicate>();
+        var functionConvertTzDataCriacao = builder.function(
+                "convert_tz", Date.class,
+                root.get("dataCriacao"),
+                builder.literal("+00:00"),
+                builder.literal(timeOffSet));
 
-        var functionDateDataCriacao = builder.function("date", LocalDate.class, root.get("dataCriacao"));
+        var functionDateDataCriacao = builder.function("date", LocalDate.class, functionConvertTzDataCriacao);
 
         var selection = builder.construct(VendaDiaria.class,
                 functionDateDataCriacao,
